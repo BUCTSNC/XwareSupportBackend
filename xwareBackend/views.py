baf71be35eea3dabb4c12d7f60b4f2282fbe8fa9
jhas <2019040504@mail.buct.edu.cn>
2021-02-23 19:15:14 +0800

    hello测试

--- xwareBackend/views.py
+++ xwareBackend/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import json
 # Create your views here.
+
+
+def hello(request):
+    res = {"code":200,"msg":"hello"}
+    return HttpResponse(json.dumps(res),content_type="text/json")
